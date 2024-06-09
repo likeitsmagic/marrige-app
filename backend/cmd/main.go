1af5a479fb0915ffdfcffdb8c7ca0e8650334d74
Danil Baltsevich <baltsevich@mindbox.cloud>
2024-06-09 23:43:54 +0300

    fix: fix nginx config

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
+	r.Group("/api").GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
