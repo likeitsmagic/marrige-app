d4d6c5a0934ae5967aa09051d78d3bd07cf270b9
Данил Балцевич <35452155+BaltsevichDanil@users.noreply.github.com>
2024-10-02 22:55:10 +0300

    feat: add frontend instructions (#25)

---
+++ README.md
@@ -0,0 +1,4 @@
+1. Установить nvm - https://formulae.brew.sh/formula/nvm (если нет brew, то установить brew - https://brew.sh/)
+2. Установить node - `nvm install 22.4`
+3. Установить зависимости - `npm ci`
+4. Настроить eslint и prettier для ide ([vscode eslint](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint), [vscode prettier](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode))
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get<ConfigService>(ConfigService);
-
   app.enableCors({
-    origin:
-      configService.get('NODE_ENV') === 'development'
-        ? 'http://localhost:5173'
-        : undefined,
+    origin: ['https://moyasvadba.space', 'http://localhost:5173'],
     credentials: true,
   });
 
