053282dc164bb36031b7460b60b7fcc98e285438
Danil Baltsevich <gnngr333@gmail.com>
2024-10-04 17:55:17 +0300

    feat add auth id

--- frontend/src/features/Auth/compoenents/Yandex/YandexAuth.tsx
+++ frontend/src/features/Auth/compoenents/Yandex/YandexAuth.tsx
@@ -1,33 +1,32 @@
 import { useEffect } from "react";
 
 export const YandexAuth = () => {
-    useEffect(() => {
-        // @ts-expect-error fix types latter
-       if (window.YaAuthSuggest) {
-        // eslint-disable-next-line 
-        (window as any).YaAuthSuggest.init(
-            {
-              client_id: "c20e0af991c84269be34f885db675513",
-              response_type: "token",
-              redirect_uri: "https://moyasvadba.space/verify_yid"
-            },
-            "https://moyasvadba.space",
-            {
-              view: "button",
-              parentId: "yandex-passport-auth-container",
-              buttonSize: 'xs',
-              buttonView: 'main',
-              buttonTheme: 'light',
-              buttonBorderRadius: "8",
-              buttonIcon: 'ya',
-            }
-          )
-          .then(({handler}: {handler: () => unknown}) => handler())
-          .then((data: unknown) => console.log('Сообщение с токеном', data))
-          .catch((error: unknown) => console.log('Обработка ошибки', error))
-       }
+	useEffect(() => {
+		// @ts-expect-error fix types latter
+		if (window.YaAuthSuggest) {
+			// eslint-disable-next-line
+			(window as any).YaAuthSuggest.init(
+				{
+					client_id: "c20e0af991c84269be34f885db675513",
+					response_type: "token",
+					redirect_uri: "https://moyasvadba.space/verify_yid",
+				},
+				"https://moyasvadba.space",
+				{
+					view: "button",
+					parentId: "yandex-passport-auth-container",
+					buttonSize: "xs",
+					buttonView: "main",
+					buttonTheme: "light",
+					buttonBorderRadius: "8",
+					buttonIcon: "ya",
+				},
+			)
+				.then(({ handler }: { handler: () => unknown }) => handler())
+				.then((data: unknown) => console.log("Сообщение с токеном", data))
+				.catch((error: unknown) => console.log("Обработка ошибки", error));
+		}
+	}, []);
 
-    }, []);
-
-    return <div id="yandex-passport-auth-container"></div>
-}
+	return <div id="yandex-passport-auth-container"></div>;
+};
