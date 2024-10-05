7bb777831f9e850bf918259bd825319e39996573
Danil Baltsevich <gnngr333@gmail.com>
2024-10-05 23:52:10 +0300

    fix

--- backend/src/campaigns/campaigns.service.ts
+++ backend/src/campaigns/campaigns.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCampaignDto } from './dto/create-campaign.dto';
 import { Campaign } from './entities/campaign.entity';
+import { omit } from 'lodash';
 
 @Injectable()
 export class CampaignsService {
@@ -19,10 +20,11 @@ export class CampaignsService {
     });
 
     if (campaign) {
+      const newData = omit(createCampaignDto, ['socialMedias']);
       return this.campaignRepository.update(
         { id: campaign.id },
         {
-          ...createCampaignDto,
+          ...newData,
         },
       );
     }
--- frontend_business/src/features/Business/schema.ts
+++ frontend_business/src/features/Business/schema.ts
@@ -2,25 +2,42 @@ import { SocialMediaPlatformEnum } from "src/core/enums/socialMediaPlatform.enum
 import { WeddingVendorTypeEnum } from "src/core/enums/weddingVendorType.enum";
 import { z } from "zod";
 
-export const schema = z.object({
-	name: z.string().min(1).max(70),
-	description: z.string().max(600).optional(),
-	phone: z.string().optional(),
-	type: z.nativeEnum(WeddingVendorTypeEnum),
-	images: z.array(z.string()),
-	address: z.string().optional(),
-	location: z.object({
-		type: z.literal("Point"),
-		coordinates: z.array(z.number()),
-	}),
-	minPrice: z.number().max(9999999999),
-	maxPrice: z.number().max(9999999999),
-	socialMedias: z.array(
-		z.object({
-			platform: z.nativeEnum(SocialMediaPlatformEnum),
-			link: z.string().url(),
+export const schema = z
+	.object({
+		name: z.string().min(1).max(70),
+		description: z.string().max(600).optional(),
+		phone: z.string().optional(),
+		type: z.nativeEnum(WeddingVendorTypeEnum).superRefine((value, ctx) => {
+			if (value === WeddingVendorTypeEnum.NONE) {
+				ctx.addIssue({
+					code: z.ZodIssueCode.custom,
+					message: "Type cannot be NONE",
+				});
+			}
 		}),
-	),
-});
+		images: z.array(z.string()),
+		address: z.string().optional(),
+		location: z.object({
+			type: z.literal("Point"),
+			coordinates: z.array(z.number()),
+		}),
+		minPrice: z.number().max(9999999999),
+		maxPrice: z.number().max(9999999999),
+		socialMedias: z.array(
+			z.object({
+				platform: z.nativeEnum(SocialMediaPlatformEnum),
+				link: z.string().url(),
+			}),
+		),
+	})
+	.superRefine((value, ctx) => {
+		if (value.minPrice && value.maxPrice && value.minPrice > value.maxPrice) {
+			ctx.addIssue({
+				path: ["minPrice"],
+				code: z.ZodIssueCode.custom,
+				message: "Min price cannot be greater than max price",
+			});
+		}
+	});
 
 export type BusinessValues = z.infer<typeof schema>;
